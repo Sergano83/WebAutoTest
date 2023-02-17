1f6f888d0647c62a0c5d240a16631264f25fc64d
sergano <sergano@list.ru>
2023-02-17 11:44:42 +0300

    Урок 5

--- src/test/java/org/example/DZ4/TriangleTest.java
+++ src/test/java/org/example/DZ4/TriangleTest.java
@@ -1,21 +1,28 @@
 package org.example.DZ4;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TriangleTest {
 
-    @Test
-    public class TriangleSquareCalculationTest {
-        @Test
-        @DisplayName("Первая позитивная проверка")
-        void positiveSquareCalcTest1() throws Exception {
-            double result = calcAreaTriangle(2, 2, 2);
-            Assertions.assertEquals(1.7320508075688772, result);
+public class TriangleTest extends Triangle {
+
+
+
+        private double calcAreaTriangle(int a, int b, int c) {
+            return 0;
         }
 
         @Test
+        @DisplayName("Первая позитивная проверка")
+        void positiveSquareCalcTest1() throws Exception {
+
+            double result = calcAreaTriangle (2, 2, 2);
+            Assertions.assertEquals(1.7320508075688772, result);
+        }
+
+
+
+    @Test
         @DisplayName("Вторая позитивная проверка")
         void positiveSquareCalcTest2() throws Exception {
             double result = calcAreaTriangle(3, 4, 5);
@@ -26,11 +33,13 @@ public class TriangleTest {
         @DisplayName("Третья позитивная проверка")
         void positiveSquareCalcTest3() throws Exception {
             double result = calcAreaTriangle(5, 6, 7);
-            Assertions.assertEquals(14.696938456699069, result);
+            Assertions.assertEquals(0, result);
         }
 
 
-    }
+
+
+
 }
 
 
---
+++ src/test/java/org/example/DZ5/AbstractTest.java
@@ -0,0 +1,42 @@
+package org.example.DZ5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class AbstractTest {
+    public static WebDriver driver;
+
+    @BeforeAll
+    static void init() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://www.livejournal.com/");
+    }
+
+
+        @AfterAll
+        static void close(){
+       driver.quit();
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
+
+
+
